3a046e99ea4321d8577d6ab39f81a34b61d458bf
De Yu <de_yu@cable.comcast.com>
2018-02-22 23:18:39 -0500

    Added method to sign up and sign in.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+
+  def full_name
+    "#{first_name.try(:capitalize)} #{last_name.try(:capitalize)}"
+  end
+end
