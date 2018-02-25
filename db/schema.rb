2df01f8931fdc67fa6053f5319f04c6b72dd02c0
sa000 <alamsakib@gmail.com>
2018-02-25 00:08:36 -0600

    Dares DB attempt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223025412) do
+ActiveRecord::Schema.define(version: 20180223225550) do
+
+  create_table "dares", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "dare_giver_id"
+    t.integer  "dare_recepient_id"
+    t.integer  "upvote_count"
+    t.boolean  "completed"
+    t.text     "description",       limit: 65535
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["dare_giver_id"], name: "index_dares_on_dare_giver_id", using: :btree
+    t.index ["dare_recepient_id"], name: "index_dares_on_dare_recepient_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
